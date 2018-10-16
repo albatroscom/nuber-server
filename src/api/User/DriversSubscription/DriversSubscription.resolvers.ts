6a16508dfeb24c35f5157f14254c3949174aa6ee
seonggu.baek <seonggu.baek@nexstreaming.com>
2018-10-16 19:58:01 +0900

    DriversSubscription Part One

---
+++ src/api/User/DriversSubscription/DriversSubscription.resolvers.ts
@@ -0,0 +1,13 @@
+import { Resolvers } from "../../../types/resolvers";
+
+const resolvers: Resolvers = {
+    Subscription: {
+        DriversSubscription: {
+            subscribe: (_, __, { pubSub }) => {
+                return pubSub.asyncIterator("driverUpdate"); // driverUpdate 라는 채널의 변화를 관찰한다.
+            }
+        }
+    }
+};
+
+export default resolvers;
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
  import cors from "cors";
- import { GraphQLServer } from "graphql-yoga";
+ import { GraphQLServer, PubSub } from "graphql-yoga";
  import helmet from "helmet";
  import logger from "morgan";
  import schema from "./schema";
 import decodeJWT from "./utils/decodeJWT";
 import { NextFunction, Response} from "express";
  
-class App {
+class App { 
     public app: GraphQLServer;
+    public pubSub: any;
     constructor() {
+        this.pubSub = new PubSub();  // this PubSUb is a kind of Demo version, use for Only Test Mode
+        this.pubSub.ee.setMaxListeners(99); // graphql-yoga에서 pubSub를 구현할때 메모리 누수가 생긴다. 그래서 이 구문을 추가해준다.(2018-10-16 현재)
         this.app = new GraphQLServer({
             schema : schema,
             context : req => {
                 return {
-                    req : req.request
+                    req : req.request,
+                    pubSub : this.pubSub
                 }
             }
         });
