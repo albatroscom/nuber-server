ad7f55d51159417a6925316ea702b6c885df6538
albatroscom <albatroscom@naver.com>
2018-11-20 00:50:16 +0900

    UpdateRideStatus Resolver Part One

--- README.md
+++ README.md
@@ -37,7 +37,8 @@ Server for the (N)uber Clone Course on Nomad Academy, GraphQL, Typescript, NodeJ
 -  [x] Subscribe to Nearby Drivers
 -  [x] Request a Ride
 -  [x] Get Nearby Rides Requests
--  [ ] Subscribe to Nearby Ride Requests
+-  [x] Subscribe to Nearby Ride Requests
+-  [ ] Update Ride Status
 -  [ ] Subscribe to Ride Status
 -  [ ] Get Chat Room Messages
 -  [ ] Subscribe to Chat Room Messages
--- src/api/Ride/RequestRide/RequestRide.resolver.ts
+++ src/api/Ride/RequestRide/RequestRide.resolver.ts
@@ -8,20 +8,30 @@ const resolvers : Resolvers = {
     Mutation: {
         RequestRide: privateResolver(async(_, args: RequestRideMutationArgs, {req, pubSub}) : Promise<RequestRideResponse> => {
             const user: User = req.user;
-            try {
-                const ride = await Ride.create({ ...args, passenger: user }).save();
-                pubSub.publish("rideRequest", { NearbyRideSubscription: ride});
-                return {
-                    ok: true,
-                    error: null,
-                    ride
-                }
-            } catch (error) {
+            if(!user.isRiding) {
+                try {
+                    const ride = await Ride.create({ ...args, passenger: user }).save();
+                    pubSub.publish("rideRequest", { NearbyRideSubscription: ride});
+                    user.isRiding = true;
+                    user.save();
+                    return {
+                        ok: true,
+                        error: null,
+                        ride
+                    };
+                } catch (error) {
+                    return {
+                        ok: false,
+                        error: error.message,
+                        ride: null
+                    };  
+                } 
+            } else {
                 return {
                     ok: false,
-                    error: error.message,
+                    error: "You can't request two rides",
                     ride: null
-                }
+                };
             }
         })
     }
